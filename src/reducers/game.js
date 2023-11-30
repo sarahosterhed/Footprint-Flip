c289bdc9ac3ab205750fb6396c2b01920d67897a
Naima Malik <noma_m@hotmail.com>
2023-11-30 12:04:32 +0100

    Moved cards state to reducers

--- src/reducers/game.js
+++ src/reducers/game.js
@@ -1,108 +1,109 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const products = [
-    {
-        "id": 1,
-        "name": "Smartphone",
-        "co2": "18kg",
-        "img": "./assets/smartphone.svg"
-    },
-    {
-        "id": 2,
-        "name": "Jeans",
-        "co2": "22kg",
-        "img": "./assets/jeans.svg"
-    },
-    {
-        "id": 3,
-        "name": "Cotton T-shirt",
-        "co2": "6kg",
-        "img": "./assets/"
-    },
-    {
-        "id": 4,
-        "name": "Bicycle",
-        "co2": "7.5kg",
-        "img": "./assets/"
-    },
-    {
-        "id": 5,
-        "name": "Sneakers",
-        "co2": "11.5kg",
-        "img": "./assets/chair.svg"
-    },
-    {
-        "id": 6,
-        "name": "European Flight",
-        "co2": "150kg",
-        "img": "./assets/phone.svg"
-    },
-    {
-        "id": 7,
-        "name": "Bus",
-        "co2": "0.2kg",
-        "img": "./assets/phone.svg"
-    },
-    {
-        "id": 8,
-        "name": "Train",
-        "co2": "0.1kg",
-        "img": "./assets/phone.svg"
-    },
-    {
-        "id": 9,
-        "name": "Car",
-        "co2": "0.2kg",
-        "img": "./assets/phone.svg"
-    },
-    {
-        "id": 10,
-        "name": "Coffee Consumption",
-        "co2": "1.5kg",
-        "img": "./assets/phone.svg"
-    },
-    {
-        "id": 11,
-        "name": "Vegetarian Food",
-        "co2": "48kg",
-        "img": "./assets/vegetarian.svg"
-    },
-    {
-        "id": 12,
-        "name": "Meat based diet",
-        "co2": "77kg",
-        "img": "./assets/meat.svg"
-    }
-]
-
+  {
+    id: 1,
+    name: "Smartphone",
+    co2: "18kg",
+    img: "./assets/smartphone.svg",
+  },
+  {
+    id: 2,
+    name: "Jeans",
+    co2: "22kg",
+    img: "./assets/jeans.svg",
+  },
+  {
+    id: 3,
+    name: "Cotton T-shirt",
+    co2: "6kg",
+    img: "./assets/",
+  },
+  {
+    id: 4,
+    name: "Bicycle",
+    co2: "7.5kg",
+    img: "./assets/",
+  },
+  {
+    id: 5,
+    name: "Sneakers",
+    co2: "11.5kg",
+    img: "./assets/chair.svg",
+  },
+  {
+    id: 6,
+    name: "European Flight",
+    co2: "150kg",
+    img: "./assets/phone.svg",
+  },
+  {
+    id: 7,
+    name: "Bus",
+    co2: "0.2kg",
+    img: "./assets/phone.svg",
+  },
+  {
+    id: 8,
+    name: "Train",
+    co2: "0.1kg",
+    img: "./assets/phone.svg",
+  },
+  {
+    id: 9,
+    name: "Car",
+    co2: "0.2kg",
+    img: "./assets/phone.svg",
+  },
+  {
+    id: 10,
+    name: "Coffee Consumption",
+    co2: "1.5kg",
+    img: "./assets/cup.svg",
+  },
+  {
+    id: 11,
+    name: "Vegetarian Food",
+    co2: "48kg",
+    img: "./assets/vegetarian.svg",
+  },
+  {
+    id: 12,
+    name: "Meat based diet",
+    co2: "77kg",
+    img: "./assets/meat.svg",
+  },
+];
 
 const initialState = {
-    products,
+  products,
 };
 
 export const game = createSlice({
-    name: "game",
-    initialState,
-    reducers: {
-        moveCard: (state, action) => {
-            const { from, to } = action.payload;
-            const updatedProducts = [...state.products];
-            const [moveCard] = updatedProducts.splice(from, 1);
-            updatedProducts.splice(to, 0, moveCard);
+  name: "game",
+  initialState,
+  reducers: {
+    moveCard: (state, action) => {
+      const { from, to } = action.payload;
+      const updatedProducts = [...state.products];
+      const [moveCard] = updatedProducts.splice(from, 1);
+      updatedProducts.splice(to, 0, moveCard);
 
-            return {
-                ...state,
-                products: updatedProducts,
-            };
-        },
-
-        restart: () => {
-            return initialState;
-        },
+      return {
+        ...state,
+        products: updatedProducts,
+      };
     },
+
+    updateCards: (state, action) => {
+      state.products = action.payload;
+    },
+
+    restart: () => {
+      return initialState;
+    },
+  },
 });
 
-export const {
-    moveCard
-} = game.actions;
+export const { moveCard, updateCards } = game.actions;
 export default game.reducer;
