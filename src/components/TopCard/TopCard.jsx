6392cc109b44031f83fddf521ce4da67011b8983
Naima Malik <noma_m@hotmail.com>
2023-12-04 20:23:10 +0100

    change background color based on incorrect or correct position

--- src/components/TopCard/TopCard.jsx
+++ src/components/TopCard/TopCard.jsx
@@ -2,13 +2,15 @@
 import React from "react";
 import "./TopCard.css";
 
-const TopCard = ({ card, droppedCard, getImagePath }) => (
+const TopCard = ({ card, droppedCard, getImagePath, color }) => (
   // <div className="top-container">
   <div
     id={card.id}
     className="top-container"
     style={{
       borderLeft: card.id === droppedCard?.id ? "30px dashed black" : "",
+      backgroundColor:
+        color === "green" ? "green" : color === "red" ? "red" : "",
     }}
   >
     <p className="card-heading">{card.name}</p>
