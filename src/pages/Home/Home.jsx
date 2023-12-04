bdecc6e2a9589eeeef578d1f515d7a7dbcc4cd4e
Sarah <sarah.osterhed@gmail.com>
2023-12-04 09:55:35 +0100

    updated navbar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,23 +1,26 @@
-import { useNavigate} from "react-router-dom";
-import "./Home.css";
+import { useNavigate } from "react-router-dom";
+import "./home.css";
 import Footer from "../../components/Footer/Footer";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-    const navigate = useNavigate();
-    const handleNavigation = () => {
-       navigate("/gamepage");
-    }
+  const navigate = useNavigate();
+  const handleNavigation = () => {
+    navigate("/gamepage");
+  }
   return (
     <div className="home-container home-page">
       <section >
-     <h1 className="heading">Welcome to Card Game</h1>
-     <h1 className="heading">Play and Learn about products impact to environment!</h1>
-     <button className="start-button" onClick={handleNavigation}>Let start</button>
-     <p className="instruction-text">CHECKOUT INSTRUCTIONS</p>
-     <Footer /> 
-     </section>
+        <h1 className="heading">Welcome to Card Game</h1>
+        <h1 className="heading">Play and Learn about products impact to environment!</h1>
+        <button className="start-button" onClick={handleNavigation}>Let start</button>
+        <Link to="/instructions">
+          <p className="instruction-text" >CHECKOUT INSTRUCTIONS</p>
+        </Link>
+        <Footer />
+      </section>
     </div>
   )
 }
 
-export default Home
+export default Home;
