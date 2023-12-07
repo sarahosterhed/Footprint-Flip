c7dbb4c4976c5a324bec367270e2a204118f4483
Priya Pradhan <102596886+Priya-Pdh@users.noreply.github.com>
2023-12-07 08:32:47 +0100

    added minons img

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
 import "../PageNotFound/PageNotFound.css";
-// import minion from "./minions.png";
+import minion from "../../assets/minion.png";
 const PageNotFound = () => {
   return (
     <div className="page-not-found">
       <h1>Page Not Found. Error 404</h1>
-      {/* <img src={minion} alt="minion" /> */}
+      <img src={minion} alt="minion" />
       <Link to="/">Please Go to Home Page</Link>
     </div>
   );
