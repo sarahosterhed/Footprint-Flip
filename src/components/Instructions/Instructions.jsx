a14ea34a8ba580f8c48aa37496174a48fcd0f99d
Naima Malik <noma_m@hotmail.com>
2023-12-10 10:05:03 +0100

    Add note in the instructions page about emissions

--- src/components/Instructions/Instructions.jsx
+++ src/components/Instructions/Instructions.jsx
@@ -1,11 +1,10 @@
 import { useNavigate } from "react-router-dom";
 import "./Instructions.css";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 
 const Instructions = () => {
   const navigate = useNavigate();
   const { t } = useTranslation();
-
 
   const playGame = () => {
     navigate("/gamepage");
@@ -14,23 +13,20 @@ const Instructions = () => {
   return (
     <>
       <div className="instructions">
-    <h1 className="instruction-heading">{t('instr')}</h1>
+        <h1 className="instruction-heading">{t("instr")}</h1>
         <ol>
-          <li>
-           {t('step_1')}
-          </li>
-          <li>
-          {t('step_2')}
-          </li>
-          <li>
-          {t('step_3')}
-          </li>
+          <li>{t("step_1")}</li>
+          <li>{t("step_2")}</li>
+          <li>{t("step_3")}</li>
         </ol>
-        <p className="note">
-        {t('note')}
+        <p className="note">{t("note")}</p>
+        <p className="note" style={{ textAlign: "left", margin: "20px" }}>
+          <span style={{ fontWeight: "bold" }}>Note:</span> The measurement of
+          emissions stated on each card for this game are not the exact number
+          of emissions, rather just an average calculation.
         </p>
         <button className="start-button" onClick={playGame}>
-        {t('play_button')}
+          {t("play_button")}
         </button>
       </div>
     </>
