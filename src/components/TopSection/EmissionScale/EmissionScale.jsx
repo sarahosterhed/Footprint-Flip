c34355ec46279a7d717202d4ad5d3b2a08f4ec36
Priya Pradhan <102596886+Priya-Pdh@users.noreply.github.com>
2023-12-13 19:42:42 +0100

    translated the info text in game page

--- src/components/TopSection/EmissionScale/EmissionScale.jsx
+++ src/components/TopSection/EmissionScale/EmissionScale.jsx
@@ -1,18 +1,20 @@
-import './EmissionScale.css'
+import "./EmissionScale.css";
+import { useTranslation } from "react-i18next";
 
 const EmissionScale = () => {
-    return (
-        <>
-        <div>
-        <p className="info-text">Drag the Bottom Cards to Top Board and Order from Lowest to Highest Emission</p>
-            <div className="scale">
-                <div className="arrow left"></div>
-                <div className="horizontal-line"></div>
-                <div className="arrow right"></div>
-            </div>
-            </div>
-        </>
-    )
-}
+  const { t } = useTranslation();
+  return (
+    <>
+      <div>
+        <p className="info-text">{t("info_text")}</p>
+        <div className="scale">
+          <div className="arrow left"></div>
+          <div className="horizontal-line"></div>
+          <div className="arrow right"></div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default EmissionScale;
